45eda8f1943b99ad4c5705429dbb85fff85b33a0
qin.yifan <jim.qinyifan@hotmail.com>
2021-10-04 10:36:10 -0400

    format

--- Practices/Dynamic Programming/gridTravel.js
+++ Practices/Dynamic Programming/gridTravel.js
@@ -6,10 +6,10 @@
 
 
 const gridTraveler = (m, n) => {
-    if (m === 1 && n === 1) return 1;
-    if (m === 0 || n === 0) return 0;
-    return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
-    
+  if (m === 1 && n === 1) return 1;
+  if (m === 0 || n === 0) return 0;
+  return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+
 }
 
 
@@ -19,12 +19,12 @@ const gridTraveler = (m, n) => {
 
 
 const newgridTraveler = (m, n, memo = {}) => {
-    const key = m + ',' + n;
-    if (key in memo) return memo[key]; 
-    if (m === 1 && n === 1) return 1;
-    if (m === 0 || n === 0) return 0;
-    memo[key] =  newgridTraveler(m - 1, n, memo) + newgridTraveler(m, n - 1, memo);
-    return memo[key];
+  const key = m + ',' + n;
+  if (key in memo) return memo[key];
+  if (m === 1 && n === 1) return 1;
+  if (m === 0 || n === 0) return 0;
+  memo[key] = newgridTraveler(m - 1, n, memo) + newgridTraveler(m, n - 1, memo);
+  return memo[key];
 }
 
 console.log(newgridTraveler(18, 18));
