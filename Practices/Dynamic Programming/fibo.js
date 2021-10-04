8ac9fa436d10d5c3455d4bacb53332e89f9184ce
qin.yifan <jim.qinyifan@hotmail.com>
2021-10-04 09:09:04 -0400

    update todolist

--- Practices/Dynamic Programming/fibo.js
+++ Practices/Dynamic Programming/fibo.js
@@ -1,10 +1,10 @@
 // JavaScript source code
 
-// get the nth fib number 
+// get the nth fib number
 const fib = (n) => {
-    if (n <= 2) return 1;
-    n = fib 
-    return fib(n - 1) + fib(n - 2);
+  if (n <= 2) return 1;
+  n = fib
+  return fib(n - 1) + fib(n - 2);
 };
 
 
@@ -15,13 +15,13 @@ const fib = (n) => {
 
 
 const newfib = (n, memo = {}) => {
-    if (n in memo) return memo[n];
-    if (n <= 2) return 1;
-    memo[n] = newfib(n - 1, memo) + newfib(n - 2, memo);
-    return memo[n];
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+  memo[n] = newfib(n - 1, memo) + newfib(n - 2, memo);
+  return memo[n];
 }
 
 
-console.log(newfib(6));    //8
-console.log(newfib(11));   //89
-console.log(newfib(50));   // 12586269025
+console.log(newfib(6)); //8
+console.log(newfib(11)); //89
+console.log(newfib(50)); // 12586269025
